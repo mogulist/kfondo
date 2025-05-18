2ed714cbd64e4a305b063c4523d92606b76abf4f
Herbert Lim <mogulist@naver.com>
2025-05-18 23:28:57 +0900

    E2E 테스트를 위해 PlayWright 설치하고 테스트 추가

---
+++ e2e/find-by-record.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+test("2025 홍천 메디오폰도 04132855 기록은 순위 496", async ({ page }) => {
+  await page.goto("/hongcheon/mediofondo/2025/find-by-record/04132855");
+  await expect(page.getByText("순위: 496위")).toBeVisible();
+});
+
+test("2025 홍천 그란폰도 05253137 기록은 순위 541", async ({ page }) => {
+  await page.goto("/hongcheon/granfondo/2025/find-by-record/05253137");
+  await expect(page.getByText("순위: 541위")).toBeVisible();
+});
+
+test("2025 양양 그란폰도 07124578 기록은 순위 389", async ({ page }) => {
+  await page.goto("/yangyang/granfondo/2025/find-by-record/07124578");
+  await expect(page.getByText("순위: 389위")).toBeVisible();
+});
+
+test("2025 양양 메디오폰도 03384028 기록은 순위 410", async ({ page }) => {
+  await page.goto("/yangyang/mediofondo/2025/find-by-record/03384028");
+  await expect(page.getByText("순위: 410위")).toBeVisible();
+});
